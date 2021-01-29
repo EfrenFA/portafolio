8d22b7a784bf58bf819d870d26b3134dc13f9249
Efren Flores Arenas <efren.flores@alumno.buap.mx>
2021-01-28 21:53:40 -0600

    Fin seccion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { descripcionProducto } from 'src/app/interfaces/producto-descripcion.interface';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+ 
+  producto!: descripcionProducto;
+  id!: string;
+  //producto: descripcionProducto;
 
-  constructor() { }
+  constructor( private route: ActivatedRoute,
+               public productoServicio: ProductosService) { }
 
   ngOnInit(): void {
+   
+
+    this.route.params.subscribe( parametros =>{
+      //console.log(parametros['id']);
+      this.productoServicio.getProducto(parametros['id'])
+      .subscribe( ( response: descripcionProducto ) => {
+        this.id = parametros['id'];
+        this.producto = response;
+        //console.log(response);
+
+      });
+    });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,16 +1,17 @@
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { descripcionProducto } from '../interfaces/producto-descripcion.interface';
 import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductosService {
 
+export class ProductosService {
   cargando = true;
   productos: Producto[]=[];
-
+  productosf: Producto[]=[];
   constructor( private http:HttpClient) { 
     this.cargarProductos();
   }
@@ -18,14 +19,65 @@ export class ProductosService {
 
 private cargarProductos(){
 
-  this.http.get('https://appefren.firebaseio.com/productos_idx.json')
-  .subscribe( (response: any )  => {
 
-    console.log(response);
-    this.productos = response;
-    this.cargando = false;
+  return new Promise( (resolve, reject )  => {
+    this.http.get('https://appefren.firebaseio.com/productos_idx.json')
+             .subscribe( (response: any )  => {
+             //console.log(response);
+             this.productos = response;
+             this.cargando = false;
+             resolve;
+  });
+
+  
 
   });
+
+}
+
+getProducto(id: string){
+
+  return this.http.get<descripcionProducto>(`https://appefren.firebaseio.com/productos/${ id }.json`);
+
+}
+
+buscarProducto( termino: string ){
+
+
+  if (this.productos.length == 0){
+    //Cargar Productos
+     this.cargarProductos().then( () => {
+        //Cargar Los Productos
+        //Aplicar el filtro
+        this.filtrarProductos(termino);
+     });
+  }else{
+    //Cargar el Filtro
+    this.filtrarProductos(termino);
+  }
+
+
+  this.productosf = this.productos.filter(producto =>{
+       return true;
+  });
+
+  //console.log(this.productosf);
+
+}
+
+private filtrarProductos(termino: string){
+
+  this.productosf = [];
+  termino = termino.toLocaleLowerCase();
+  
+    this.productos.forEach( prod => {
+
+      const titulolower =  prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino) >= 0 || titulolower.indexOf(termino) >= 0){
+          this.productosf.push(prod);
+      }
+    });
 
 }
 
